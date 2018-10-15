1bca1a7d9676033cec396a18894bfdb8e935a414
LG Family <ruoqing.zhao@gmail.com>
2018-10-15 15:51:33 +0800

    blablablaboop

---
+++ Notable/subjectContentViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Notable
+//
+//  Created by 1010happy on 14/10/2018.
+//  Copyright © 2018 Yi Lin and Janessa :D. All rights reserved.
+//
+
+import UIKit
+
+class subjectContentViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
